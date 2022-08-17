155acc5b09380edef2494cc3daeecf4d84075070
sdam abdi <sdamabdi@sdams-MacBook-Pro.local>
2022-08-17 18:18:54 -0500

    k

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -24,7 +24,7 @@ sessions.post('/login', (req, res) => {
                 // add the user to our session
                 req.session.currentUser = foundUser
                 // redirect back to our home page
-                res.redirect('/')
+                res.redirect('/homepage')
             } else {
                 // passwords do not match
                 res.send('<a href="/"> password does not match </a>')
