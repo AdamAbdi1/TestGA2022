fe10d041d4c779d9c1593a6ec8306a8aece17234
sdam abdi <sdamabdi@sdams-MacBook-Pro.local>
2022-08-17 14:43:18 -0500

    k

--- server.js
+++ server.js
@@ -51,7 +51,6 @@ app.use(express.static('public'));
 // populates req.body with parsed info from forms - if no data from forms will return an empty object {}
 app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
 app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
-app.use(express.session());
 
 //use method override
 app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
